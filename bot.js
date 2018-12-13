5253abbc354b0f1cb542346b20638efb43dce556
nedal235 <38614159+nedal235@users.noreply.github.com>
2018-12-13 19:02:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,6 +43,28 @@ reaction2.on("collect", r => {
     message.channel.send(" كيف اقدر اساعدك!!");
     } 
 	return;
+	
+		if(message.content.startsWith(prefix + "id")) {
+const embed = new Discord.RichEmbed()
+  .setTitle('iD')
+  .setDescription(`معرفة الاي دي`)
+  .setThumbnail(`http://s.ppy.sh/a/${user.id}}`)
+  .setColor("#D0436A")
+  .addField('Nickname', user.name, true)
+  .addField('PP', Math.round(user.pp.raw), true)
+  .addField('Rank', user.pp.rank, true)
+  .addField('Level', Math.round(user.level), true)
+  .addBlankField()
+  .addField('Country', user.country, true)
+  .addField('Country Rank', user.pp.countryRank, true)
+  .addField('Playcount', user.counts.plays, true)
+  .addField('Accuracy', `${user.accuracyFormatted}`, true)
+  .setFooter('Requested By ' + message.author.tag, message.author.avatarURL)
+  message.channel.send(embed)
+		
+		
+		
+	}
 });
 client.on('guildMemberAdd', member => {
     let channel = member.guild.channels.find('name', '✥⇣❀شــــات̲الـتـرحـيـب❀⇣✥');
@@ -82,33 +104,5 @@ client.on('guildMemberRemove', member => {
  
     });
 
-client.on('message', message => { 
-           if (message.content.startsWith(prefix + "id")) {
-     var args = message.content.split(" ").slice(1);
-     let user = message.mentions.users.first();
-     var men = message.mentions.users.first();
-        var heg;
-        if(men) {
-            heg = men
-        } else {
-            heg = message.author
-        }
-      var mentionned = message.mentions.members.first();
-         var h;
-        if(mentionned) {
-            h = mentionned
-        } else {
-            h = message.member
-        }
-               moment.locale('ar-TN');
-      var id = new  Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL) 
-    .setColor("#707070")
-    .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
-    .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)               
-    .setFooter(`GN Bot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
-    .setThumbnail(heg.avatarURL);
-    message.channel.send(id)
-}       });	
     
 	client.login(process.env.BOT_TOKEN);
